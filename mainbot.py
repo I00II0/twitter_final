a76d02c7013e2ced718ec0c189a04842133991dc
I00II0 <70446142+I00II0@users.noreply.github.com>
2020-10-24 19:23:19 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : ameen|{follower_count(user)} Followers')
+    user = api.get_user('RanjayDaimary')  #Twitter username
+    api.update_profile(name=f'ranjay daimary|{follower_count(user)} Followers')   #the display you want on Twitter
+    print(f'Updating Twitter Name : ranjay daimary|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
